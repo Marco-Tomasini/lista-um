66ac3abf4a7f921ad3401aff9c718ff6414a61a6
Marco_Tomasini <marco_tomasini@estudante.sesisenai.org.br>
2025-07-16 11:20:11 -0300

    UPDATE - atividade 11

---
+++ atividade-11.php
@@ -0,0 +1,37 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Palíndromo</title>
+</head>
+<body>
+    
+    <form method="POST" action="">
+        <label for="palavra">escreva uma palavra para verificar: </label>
+        <input type="text" id="palavra" name="palavra">
+        <button id="palavra_verificar" name="palavra_verificar">Verificar</button>
+    </form>
+
+
+    <?php
+
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            if(isset($_POST['palavra_verificar'])){
+                $palavra = $_POST['palavra'];
+                $palavraI = strrev($palavra);
+
+                if($palavra == $palavraI){
+                    echo "A palavra $palavra é um palíndromo";
+                } else{
+                    echo "A palavra $palavra NÃO é um palíndromo";
+                }
+
+
+            }
+        }
+
+
+    ?>
+
+</body>
+</html>
