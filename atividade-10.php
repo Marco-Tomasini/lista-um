f963b971c53048c2f99fdfe50c7d99c1547c10cd
Marco_Tomasini <marco_tomasini@estudante.sesisenai.org.br>
2025-07-16 11:11:51 -0300

    UPDATE - exercicio 10

--- atividade-10.php
+++ atividade-10.php
@@ -7,9 +7,30 @@
 <body>
 
     <form method="POST" action="">
-        <label for="soma">Escreva dois Nºs para verificar a soma entre eles</label>
+        <label for="Fibonacci">Escreva um Nº para exibir a sequência de Fibonacci até esse número</label>
         <input type="number" id="n1" name="n1">
-        <button type="submit" name="verificar_soma">Verificar</button>
+        <button type="submit" name="verificar_Fibonacci">Verificar</button>
     </form>
 
 <?php
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        if(isset($_POST['verificar_Fibonacci'])){
+            $n1 = $_POST['n1'];
+            $fiboncci = [0,1];
+
+            for($i = $fiboncci[1]; $i <= $n1; $i++){
+
+                $fiboncci[$i + 1] = $fiboncci[$i - 1] + $fiboncci[$i];
+            }
+
+            foreach($fiboncci as $fibonccii) {
+                echo $fibonccii . "<br>";
+            }
+
+        }
+    }
+
+
+
+?>
